d45db43ae9c45fe440b651381e9aa099d395d67f
Eric Perin <eric.perin@outlook.com>
2023-06-12 23:26:27 -0300

    add prometheus-net

--- src/Example.Api/Program.cs
+++ src/Example.Api/Program.cs
@@ -1,3 +1,5 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,6 +20,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseHttpMetrics();
+app.MapMetrics();
 app.UseAuthorization();
 
 app.MapControllers();
